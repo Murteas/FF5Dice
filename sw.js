785ec0b37fec3090281f37ff311184700c356b22
David Saetrum <dsaetrum@familysearch.org>
2025-04-15 11:25:58 -0600

    Added new icon, manifest and offline cache

---
+++ sw.js
@@ -0,0 +1,20 @@
+// sw.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('ff5dice-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
